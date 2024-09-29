fb32c84a7f99d8256fd91827ec28171d81b1143c
Hurinovich_Yauheni <kwazi@tut.by>
2024-09-29 16:17:55 +0200

    подчистил полностью начальный проект

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>DDS-group</h1>
+        <h6>
+          Our company provides services for maintenance, repair, installation
+          and configuration of climate systems.
+        </h6>
+      </header>
+    </div>
+  )
+}
+
+export default App
