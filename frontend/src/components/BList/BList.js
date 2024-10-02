7d4dd38a365a9fc76e03ab4dd76a9525b83d0687
Hurinovich_Yauheni <kwazi@tut.by>
2024-10-02 10:51:42 +0200

    330

--- frontend/src/components/BList/BList.js
+++ frontend/src/components/BList/BList.js
@@ -2,9 +2,23 @@ import { useSelector } from 'react-redux'
 import './BList.css'
 
 const BList = () => {
+  const task = useSelector((state) => state.task)
   return (
     <div className="app-block book-list">
       <h2>Task list</h2>
+      {task.lenght === 0 ? (
+        <p>No task avilable</p>
+      ) : (
+        <ul>
+          {task.map((task, i) => (
+            <li key={i}>
+              <div className="book-info">
+                {++i}. {task.title} by <strong>{task.author}</strong>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
