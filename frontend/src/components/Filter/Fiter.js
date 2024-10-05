9706acfcd75b43df469bde1980f6ad30cf6a0df1
Hurinovich_Yauheni <kwazi@tut.by>
2024-10-05 14:00:43 +0200

    348 ch box

--- frontend/src/components/Filter/Fiter.js
+++ frontend/src/components/Filter/Fiter.js
@@ -2,24 +2,29 @@ import { useDispatch, useSelector } from 'react-redux'
 import {
   setTitleFilter,
   setAuthorFilter,
+  setOnlyFavoritFilter,
   resetFilters,
   selectTitleFilter,
   selectAuthorFilter,
+  selectOnlyFavoriteFilter,
 } from '../../redux/slices/filterSlice'
-
 import './Fiter.css'
 
 const Fiter = () => {
   const dispatch = useDispatch()
   const titleFilter = useSelector(selectTitleFilter)
   const authorFilter = useSelector(selectAuthorFilter)
-
+  const onlyFavoriteFilter = useSelector(selectOnlyFavoriteFilter)
   const handleTitleFilterChange = (e) => {
     dispatch(setTitleFilter(e.target.value))
   }
 
   const handleAuthorFilterChange = (e) => {
     dispatch(setAuthorFilter(e.target.value))
+  }
+
+  const handleOnlyFavoriteFilterChange = () => {
+    dispatch(setOnlyFavoritFilter())
   }
 
   const handleResetFilters = () => {
@@ -46,6 +51,18 @@ const Fiter = () => {
             onChange={handleAuthorFilterChange}
           />
         </div>
+
+        <div className="filter-group">
+          <label>
+            <input
+              type="checkbox"
+              checked={onlyFavoriteFilter}
+              onChange={handleOnlyFavoriteFilterChange}
+            />
+            Only Favorite
+          </label>
+        </div>
+
         <button type="button" onClick={handleResetFilters}>
           Reset
         </button>
--- frontend/src/redux/slices/filterSlice.js
+++ frontend/src/redux/slices/filterSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
   title: '',
   author: '',
+  onlyFavorite: false,
 }
 
 const filterSlice = createSlice({
@@ -15,14 +16,22 @@ const filterSlice = createSlice({
     setAuthorFilter: (state, action) => {
       state.author = action.payload
     },
-    resetFilters: (state) => {
+    setOnlyFavoritFilter: (state) => {
+      state.onlyFavorite = !state.onlyFavorite
+    },
+    resetFilters: () => {
       return initialState
     },
   },
 })
 
-export const { setTitleFilter, setAuthorFilter, resetFilters } =
-  filterSlice.actions
+export const {
+  setTitleFilter,
+  setAuthorFilter,
+  resetFilters,
+  setOnlyFavoritFilter,
+} = filterSlice.actions
 export const selectTitleFilter = (state) => state.filter.title
 export const selectAuthorFilter = (state) => state.filter.author
+export const selectOnlyFavoriteFilter = (state) => state.filter.onlyFavorite
 export default filterSlice.reducer
