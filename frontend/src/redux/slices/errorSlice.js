ece4e863f0a021b136e3287abc72278c2efd6cea
Hurinovich_Yauheni <kwazi@tut.by>
2024-10-06 12:00:23 +0200

    363 error slice

---
+++ frontend/src/redux/slices/errorSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = ''
+const errorSlice = createSlice({
+  name: 'error',
+  initialState: initialState,
+  reducers: {
+    setError: (state, action) => {
+      return action.payload
+    },
+    clearError: () => {
+      return initialState
+    },
+  },
+})
+
+export const { setError, clearError } = errorSlice.actions
+export const selectErrorMessage = (state) => state.error
+
+export default errorSlice.reducer
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import taskReduser from './slices/taskSlice'
 import filterReducer from './slices/filterSlice'
-
+import errorReducer from './slices/errorSlice'
 const store = configureStore({
   reducer: {
     task: taskReduser,
     filter: filterReducer,
+    error: errorReducer,
   },
 })
 
